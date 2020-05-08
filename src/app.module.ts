66bbb50a99284a33d0a71c6b4c6e2b003f15deb2
Valdir Lourenco <valdir2310@yahoo.com.br>
2020-05-08 20:31:55 -0300

    created the Dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MaratonaModule } from './maratona/maratona.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Maratona } from './maratona/maratona.entity';
 
 @Module({
   imports: [MaratonaModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
-      database: 'app.db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-   }),],
+      database: 'database.sqlite',
+      entities: [Maratona],
+   })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
